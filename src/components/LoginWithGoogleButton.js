6c07a876228206f547ed48c9864a1ce3cedc7f45
Shanu Dey <sd4shanudey@gmail.com>
2022-01-13 20:51:43 +0530

    add login with google button
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

---
+++ src/components/LoginWithGoogleButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const LoginWithGoogleButton = ({ onClickHandler }) => {
+  return (
+    <div>
+      <a
+        className='btn btn-outline-dark'
+        href='#'
+        role='button'
+        style={{ 'text-transform': 'none' }}
+        onClick={onClickHandler}
+      >
+        <img
+          width='20px'
+          style={{ marginBottom: '3px', marginRight: '5px' }}
+          alt='Google sign-in'
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png'
+        />
+        Login with Google
+      </a>
+    </div>
+  );
+};
