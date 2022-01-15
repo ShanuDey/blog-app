6793ba481927d85c251929f38e37b829c9b05dcc
Shanu Dey <sd4shanudey@gmail.com>
2022-01-15 16:19:04 +0530

    Implemented create post
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,27 +1,58 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Container, Card, Form, Button } from 'react-bootstrap';
 import { UserContext } from '../contexts/UserContext';
+import { db } from '../firebase';
+import { addDoc, collection } from 'firebase/firestore';
 
 export const CreatePost = () => {
   const { user, setUser } = useContext(UserContext);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const handleCreatePost = (event) => {
+    event.preventDefault();
+    if (user === '') {
+      alert('No valid user found, Please login again');
+    } else {
+      addDoc(collection(db, 'posts'), {
+        title: title,
+        author: user,
+        date: new Date().toLocaleDateString('en-US'),
+        body: body,
+      })
+        .then((result) => {
+          alert('Post created successfully');
+        })
+        .catch((error) => {
+          console.log('Failed to create the post', error.message);
+        });
+    }
+  };
+
   return (
     <Container>
-      <Card
-        style={{
-          marginTop: '3em',
-        }}
-      >
-        <Form>
+      <Card>
+        <Form onSubmit={handleCreatePost}>
           <Card.Body>
             <h3>Create Post</h3>
             <Card.Title>
-              <Form.Control type='text' placeholder='Title' />
+              <Form.Control
+                type='text'
+                placeholder='Title'
+                onChange={(event) => setTitle(event.target.value)}
+              />
             </Card.Title>
             <Card.Subtitle className='mb-2 text-muted'>
-              created by {user} on {new Date().toLocaleDateString('en-US')}
+              created by {user || 'Me'} on{' '}
+              {new Date().toLocaleDateString('en-US')}
             </Card.Subtitle>
             <Card.Text>
-              <Form.Control as='textarea' rows='10' placeholder='Body' />
+              <Form.Control
+                as='textarea'
+                rows='10'
+                placeholder='Body'
+                onChange={(event) => setBody(event.target.value)}
+              />
             </Card.Text>
             <Button variant='outline-success' type='submit'>
               Submit
